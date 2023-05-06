db2bb27caf28e6ae25df29646cc9b8489cf34e72
lina <lina-chalouati@github.com>
2023-05-06 13:52:41 +0100

    last update

---
+++ src/main/java/com/expo/healthcheck/service/HealthCheckService.java
@@ -0,0 +1,18 @@
+package com.expo.healthcheck.service;
+
+import lombok.Value;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+@Service
+public class HealthCheckService {
+
+}
